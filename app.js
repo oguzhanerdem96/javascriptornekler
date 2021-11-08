f7af4e44c3b1112a1e4219148bdc1b86517e758a
oguzhanerdem96 <oguzhan_erdem96@hotmail.com>
2021-11-08 19:05:28 +0300

    https://github.com/oguzhanerdem96/asalsayihesabi.git

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-let girilensayi = parseInt(prompt("lütfen bir sayi giriniz."));
-console.log("girdiğiniz sayi :" + girilensayi);
+let girilenSayi = parseInt(prompt("lütfen bir sayi giriniz örn : 10 "));
+let gosterelicekMetin = " "
+for(let i =2 ; i <= girilenSayi ; i++){
+    Asalsayimi = true;
+
+
+for(let j=2 ; j<i  ; j++){
+   if(i % j == 0 ){
+    Asalsayimi = false ;
+    break ;
+}
+} if(Asalsayimi == true){
+gosterelicekMetin = gosterelicekMetin + i + " " 
+}
+}
+console.log(gosterelicekMetin);
